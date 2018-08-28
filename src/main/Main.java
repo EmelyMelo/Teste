91a96c215bcaae3ec1dc28a80f4586cb026372c0
EMELY LAUANA RODRIGUES DE MELO <20151D12GR0049@LABS.GARANHUNS.IFPE>
2018-08-28 10:25:47 -0300

    first commit

---
+++ src/main/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+
+	@Test
+	public static void teste() {
+		// Cen�rio
+		Aluno a = new Aluno();
+		Secretaria s = new Secretaria();
+		
+		// A��o
+		double media = s.media(a);
+		//Verifica��o
+		//System.out.println(media);
+		a.avaliar(2, 8);
+		a.recuperar(4);
+		
+	}
+
+}
